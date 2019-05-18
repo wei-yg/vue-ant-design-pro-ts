830791f32329ccba3bb31bbc6f5b29946d3b7217
wyg <290483181@qq.com>
2019-05-18 10:41:13 +0800

    更新demo

--- src/router.ts
+++ src/router.ts
@@ -54,7 +54,7 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "layout" */ "./layouts/BasicLayout.vue"),
     children: [
-      // dashboard
+      //
       {
         path: "/",
         redirect: "/dashboard/analysis"
